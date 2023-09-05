69e45b0ce0176cd652a6eec781b1b97dad577829
Ronel Cruz <rcruz@wepsys.net>
2023-09-05 00:37:51 -0400

    Update branches.module.ts, branch.service.ts, and 4 more files...

--- src/app/pages/branches/branches.module.ts
+++ src/app/pages/branches/branches.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ListComponent } from './list/list.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { TasksRoutingModule } from '../tasks/tasks-routing.module';
 import { UIModule } from 'src/app/shared/ui/ui.module';
 import { NgApexchartsModule } from 'ng-apexcharts';
 import { BsDatepickerModule } from 'ngx-bootstrap/datepicker';
@@ -10,6 +9,7 @@ import { ModalModule } from 'ngx-bootstrap/modal';
 import { CKEditorModule } from '@ckeditor/ckeditor5-angular';
 import { DndModule } from 'ngx-drag-drop';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { BranchsRoutingModule } from './branches-routing.module';
 
 
 
@@ -21,7 +21,7 @@ import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
     CommonModule,
     FormsModule,
     ReactiveFormsModule,
-    TasksRoutingModule,
+    BranchsRoutingModule,
     UIModule,
     NgApexchartsModule,
     BsDatepickerModule.forRoot(),
--- src/app/pages/branches/list/list.component.ts
+++ src/app/pages/branches/list/list.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { Branch } from './list.model';
+import { BranchService } from './branch.service';
+import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
+import { FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-list',
@@ -6,5 +10,68 @@ import { Component } from '@angular/core';
   styleUrls: ['./list.component.scss']
 })
 export class ListComponent {
+  modalRef?: BsModalRef;
+  branchListForm!:FormGroup;
+  submitted: boolean = false;
+  currentPage: any;
+  constructor(public service: BranchService,private modalService: BsModalService){
 
+  }
+branches:Branch[];
+
+ngOnInit(): void {
+  //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+  //Add 'implements OnInit' to the class.
+  this.getAll();
+}
+
+  openViewModal(content: any) {
+    this.modalRef = this.modalService.show(content);
+  }
+
+  openModal(content: any) {
+    this.submitted = false;
+    this.modalRef = this.modalService.show(content, { class: 'modal-md' });
+  }
+
+   /**
+   * Form data get
+   */
+   get form() {
+    return this.branchListForm.controls;
+  }
+
+  /**
+  * Save user
+  */
+  saveUser() {
+    if (this.branchListForm.valid) {
+      this.service.saveBranch(this.branchListForm.value)
+        .subscribe({
+          next: (data) => {
+            this.getAll();
+            this.modalService.hide();
+          }
+        })
+    }
+
+    setTimeout(() => {
+      this.branchListForm.reset();
+    }, 2000);
+
+    this.submitted = true;
+  }
+
+  pageChanged(event: any) {
+    this.currentPage = event.page;
+  }
+
+  getAll() {
+    this.service.GetAll()
+      .subscribe({
+        next: (data) => {
+          this.branches = data;
+        }
+      })
+  }
 }
